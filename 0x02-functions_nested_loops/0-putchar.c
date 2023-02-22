e4f3c17fa58af2579444779ab0670c18fa262e6e
layla <jamillayla05gmail.com>
2023-02-22 07:13:13 -0800

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,23 +1,23 @@
-#include "_putchar.h"
-
+#include "main.h"
 /**
- * main - entry point
- * Prints _putchar followed by a newline
- *
- * Return: 0 (success)
+ * main - print _putchar
+ * is written by mbah
+ * Return: 0
  */
+
+
 int main(void)
 {
-	_putchar('_');
-	_putchar('p');
-	_putchar('u');
-	_putchar('t');
-	_putchar('c');
-	_putchar('h');
-	_putchar('a');
-	_putchar('r');
-	_putchar('\n');
-
-	return (0);
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+_putchar('\n');
+return (0);
 }
+
 
